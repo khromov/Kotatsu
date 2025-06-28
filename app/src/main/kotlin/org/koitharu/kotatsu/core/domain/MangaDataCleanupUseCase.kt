c94555e0e0f713734cfe030838ac0c96f81d3d92
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2025-06-28 22:25:20 +0200

    Update MangaDataCleanupUseCase.kt

--- app/src/main/kotlin/org/koitharu/kotatsu/core/domain/MangaDataCleanupUseCase.kt
+++ app/src/main/kotlin/org/koitharu/kotatsu/core/domain/MangaDataCleanupUseCase.kt
@@ -1,6 +1,9 @@
 package org.koitharu.kotatsu.core.domain
 
+import androidx.core.net.toUri
 import org.koitharu.kotatsu.core.db.MangaDatabase
+import org.koitharu.kotatsu.core.util.ext.URI_SCHEME_ZIP
+import org.koitharu.kotatsu.core.util.ext.isFileUri
 import org.koitharu.kotatsu.parsers.model.Manga
 import javax.inject.Inject
 
@@ -32,5 +35,12 @@ class MangaDataCleanupUseCase @Inject constructor(
 		// Delete edge bounds for pages that match this manga's URL pattern
 		val pattern = "%${manga.publicUrl}%"
 		database.getEdgeBoundsDao().deleteByUrlPattern(pattern)
+		
+		// For local files, also check for file+zip:// URLs since ZIP archives
+		// store pages with file+zip:// scheme instead of file:// scheme
+		if (manga.publicUrl.isNotEmpty() && manga.publicUrl.toUri().isFileUri()) {
+			val zipPattern = "%${manga.publicUrl.replace("file://", "$URI_SCHEME_ZIP://")}%"
+			database.getEdgeBoundsDao().deleteByUrlPattern(zipPattern)
+		}
 	}
 }
